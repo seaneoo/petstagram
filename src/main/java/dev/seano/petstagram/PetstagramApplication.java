090502c65a370bb0b123200d5c78753985068e49
Sean O'Connor <so@seano.dev>
2024-11-17 22:44:56 -0700

    Initial commit

---
+++ src/main/java/dev/seano/petstagram/PetstagramApplication.java
@@ -0,0 +1,12 @@
+package dev.seano.petstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetstagramApplication.class, args);
+    }
+}
