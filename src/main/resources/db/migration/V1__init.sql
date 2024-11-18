2d283e797151bac442964f16bd03f026a763e871
Sean O'Connor <so@seano.dev>
2024-11-18 13:26:51 -0700

    Update V1__init.sql
    Added 'enabled' column, will be mapped to Spring's UserDetails::isEnabled. 'chk_username_format' constraint, ensures the username column adheres to a regex pattern. 'idx_users_enabled' index, allows us to find enabled users.
    
    Signed-off-by: Sean O'Connor <so@seano.dev>

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,7 +3,10 @@ CREATE TABLE "users"
     "id"         serial PRIMARY KEY,
     "username"   varchar(20) NOT NULL UNIQUE,
     "password"   bytea       NOT NULL,
-    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "enabled"    bool        NOT NULL DEFAULT TRUE,
+    CONSTRAINT "chk_username_format" CHECK ("username" ~ '^\w+$')
 );
 
 CREATE INDEX "idx_users_username" ON "users" ("username");
+CREATE INDEX "idx_users_enabled" ON "users" ("username") WHERE "enabled" = TRUE;
