f3242766ada9fe56ef68b3eb7ae9ea83ebb680c6
Sean O'Connor <so@seano.dev>
2024-11-20 11:08:32 -0700

    Return UserResponse DTO from GET methods
    Signed-off-by: Sean O'Connor <so@seano.dev>

--- src/main/java/dev/seano/petstagram/user/UserController.java
+++ src/main/java/dev/seano/petstagram/user/UserController.java
@@ -1,10 +1,13 @@
 package dev.seano.petstagram.user;
 
+import dev.seano.petstagram.user.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -17,14 +20,14 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<String> findAll() {
-        return ResponseEntity.ok(userService.findAll().toString());
+    public ResponseEntity<List<UserResponse>> findAll() {
+        var users = userService.findAll().stream().map(UserResponse::new).toList();
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> findById(@PathVariable Long id) {
-        var user = userService.findById(id);
-        return user.map(value -> ResponseEntity.ok(value.toString()))
-                   .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<UserResponse> findById(@PathVariable Long id) {
+        var user = userService.findById(id).map(UserResponse::new);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
