a379c25d670d748b258dbcde5dc33e264a54e24e
Sean O'Connor <so@seano.dev>
2024-11-20 10:55:28 -0700

    Add `findAll` and `findById` methods
    Signed-off-by: Sean O'Connor <so@seano.dev>

--- src/main/java/dev/seano/petstagram/user/UserService.java
+++ src/main/java/dev/seano/petstagram/user/UserService.java
@@ -2,6 +2,11 @@ package dev.seano.petstagram.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Service
 public class UserService {
 
@@ -9,5 +14,13 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return StreamSupport.stream(userRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
     }
 }
