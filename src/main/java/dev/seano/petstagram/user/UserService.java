9a92ebb5e4123d8c564e77fe7d7d11252f714399
Sean O'Connor <so@seano.dev>
2024-11-30 21:01:57 -0700

    Create UserRepository::findByUsernameIgnoreCase and UserService::findByUsername, UserService::save

--- src/main/java/dev/seano/petstagram/user/UserService.java
+++ src/main/java/dev/seano/petstagram/user/UserService.java
@@ -23,4 +23,12 @@ public class UserService {
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsernameIgnoreCase(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
